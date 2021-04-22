bf7133f2b076ba3d0f1b3b744b9e480c14ddd76a
Sajawal khan <sajawalk028@gamil.com>
2021-04-22 16:21:59 -0700

    changing updated

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,20 @@
 var slide = document.getElementById("mySidepanel");
 var navRight = document.getElementById("nav-right");
-var navLinks = document.getElementById("nav-links");
+var navlinks = document.getElementById("nav-links");
 var closeLink = document.getElementById("close-nav");
-
-var downSlide = document.getElementById("popUpDown");
-var downContent = document.getElementById('popup-down');
-var downSlideClose = document.getElementById('close-icon');
-downSlide.addEventListener("click", showDownSlide);
-downSlideClose.addEventListener("click", closeDownSlide);
-
 navRight.addEventListener("click", showLinks);
 closeLink.addEventListener("click", closeLinks);
-
-
 function showDownSlide() {
     downContent.style.height = "100%";
 }
-
 function closeDownSlide() {
     downContent.style.height = "0";
 }
-
 function showLinks() {
-    navLinks.style.width = "100%";
+    navlinks.style.width = "100%";
 }
-
 function closeLinks() {
-    navLinks.style.width = "0";
+    navlinks.style.width = "0";
 }
 
 function openNav() {
